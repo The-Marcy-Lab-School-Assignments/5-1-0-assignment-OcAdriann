3b70cd65ebcef80557ea104da3d4e9fcf3a5255b
OcAdriann <ocadriannnc20@gmail.com>
2024-07-16 14:51:59 -0400

    All done

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -2,6 +2,11 @@ import getId from "../utils/getId";
 
 class CartItem {
 
+    constructor(name, price) {
+        this.id = getId()
+        this.name = name
+        this.price = price
+    }
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,35 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
-
+    #cartItems = []
+    
+    static #allCarts = []
+    constructor() {
+        this.id = getId()
+        ShoppingCart.#allCarts.push(this)
+    }
+    createItem(name, price) {
+        const createdItem = new CartItem(name, price)
+        this.#cartItems.push(createdItem)
+        return createdItem
+    }
+    getItems() {
+        return [...this.#cartItems]
+    }
+    removeItem(id) {
+        this.#cartItems.splice(this.#cartItems.findIndex((item) => item.id === id), 1)
+    }
+    getTotal() {
+        const sum = this.#cartItems.reduce((a, c) => a + c.price, 0)
+        return sum
+    }
+    static listAll() {
+        return [...ShoppingCart.#allCarts]
+    }
+    static findBy(id) {
+        const cart = ShoppingCart.#allCarts.filter((cart) => cart.id === id)
+        return cart[0]
+    }
 }
 
 export default ShoppingCart;
